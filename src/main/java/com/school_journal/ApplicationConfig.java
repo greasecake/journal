7b402c24e670a525d350e0d55aea5efde1944c3b
Vadim <kv1o@yandex.ru>
2021-11-09 15:08:37 +0300

    add dependencies and config

--- src/main/java/com/school_journal/ApplicationConfig.java
+++ src/main/java/com/school_journal/ApplicationConfig.java
@@ -1,15 +1,30 @@
 package com.school_journal;
 
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
 @Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.school_journal")
 public class ApplicationConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -17,5 +32,48 @@ public class ApplicationConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        try {
+            EmbeddedDatabaseBuilder dbBuilder = new EmbeddedDatabaseBuilder();
+            return dbBuilder.setType(EmbeddedDatabaseType.H2)
+                    .addScripts("classpath:sql/schema.sql", "classpath:sql/test-data.sql").build();
+        } catch (Exception e) {
+            logger.error("Embedded datasource cannot be created", e);
+            return null;
+        }
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        hibernateProperties.put("hibernate.format sql", true);
+        hibernateProperties.put("hibernate.use sql comments", true);
+        hibernateProperties.put("hibernate.show_sql", true);
+        hibernateProperties.put("hibernate.max fetch_depth", 3);
+        hibernateProperties.put ( "hibernate. jdbc.batch size", 10);
+        hibernateProperties.put ("hibernate.jdbc. fetch_size", 50);
+        return hibernateProperties;
+    }
+
+
+    @Bean
+    public SessionFactory sessionFactory() throws IOException {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan(
+                "com.school_journal");
+        sessionFactoryBean.setHibernateProperties(
+                hibernateProperties());
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager () throws IOException {
+    return new HibernateTransactionManager(
+            sessionFactory());
     }
 }
